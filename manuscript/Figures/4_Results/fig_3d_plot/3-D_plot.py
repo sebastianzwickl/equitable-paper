ed6806da216a7fef0960443acaf3741b9dbf5a53
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2021-11-15 10:56:26 +0100

    Add 3-D plot illustrating OC

---
+++ manuscript/Figures/4_Results/fig_3d_plot/3-D_plot.py
@@ -0,0 +1,202 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.transforms
+
+c_case_A = "#AA14F0"
+c_case_0 = "#082032"
+c_case_B = "#1C7947"
+c_case_C = "#14279B"
+c_case_D = "#00C1D4"
+
+def plt_sphere(list_center, list_radius):
+    for c, r in zip(list_center, list_radius):    
+        ax = fig.gca(projection='3d')
+
+        u, v = np.mgrid[0:2*np.pi:50j, 0:np.pi:50j]
+        x = r*np.cos(u)*np.sin(v)
+        y = r*np.sin(u)*np.sin(v)
+        z = r*np.cos(v)
+    
+        ax.plot_surface(x-c[0], y-c[1], z-c[2], color=np.random.choice(['g','b']), alpha=0.5*np.random.random()+0.5)
+
+
+
+plt.style.use(['science'])
+plt.rcParams['xtick.labelsize'] = 5
+plt.rcParams['ytick.labelsize'] = 5
+plt.rc('legend', fontsize=4)
+plt.rcParams['grid.color'] = "#C9CCD5"
+plt.rcParams['grid.linewidth'] = 0.25
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# FIRST LINE
+f_line_x = np.linspace(0, 1/3, 1000)
+f_line_y = np.linspace(0, 1/3, 1000)
+f_line_z = np.linspace(0, 1/3, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, color=c_case_A, linewidth=0.25,
+          linestyle="solid")
+
+f_line_x = np.linspace(0, 0, 1000)
+f_line_y = np.linspace(0, 1, 1000)
+f_line_z = np.linspace(0, 0, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, color=c_case_C, linewidth=0.25,
+          linestyle="solid")
+
+f_line_x = np.linspace(0, 1/2, 1000)
+f_line_y = np.linspace(0, 1/2, 1000)
+f_line_z = np.linspace(0, 0, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, color=c_case_B, linewidth=0.25,
+          linestyle="solid")
+
+f_line_x = np.linspace(0, 1/2, 1000)
+f_line_y = np.linspace(0, 0, 1000)
+f_line_z = np.linspace(0, 1/2, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, color=c_case_D, linewidth=0.25,
+          linestyle="solid")
+
+
+f_line_x = np.linspace(1/3, 1/3, 1000)
+f_line_y = np.linspace(0, 1/3, 1000)
+f_line_z = np.linspace(0, 0, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, 'gray', linestyle="dotted",
+          linewidth=0.35)
+
+f_line_x = np.linspace(1/3, 1/3, 1000)
+f_line_y = np.linspace(1/3, 1/3, 1000)
+f_line_z = np.linspace(0, 1/3, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, 'gray', linestyle="dotted",
+          linewidth=0.35)
+
+f_line_x = np.linspace(0, 1/3, 1000)
+f_line_y = np.linspace(0, 0, 1000)
+f_line_z = np.linspace(0, 0, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, 'gray', linestyle="dotted",
+          linewidth=0.35)
+
+f_line_x = np.linspace(0, 0, 1000)
+f_line_y = np.linspace(0, 0, 1000)
+f_line_z = np.linspace(0, 1, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, color=c_case_0, linewidth=0.25,
+          linestyle="solid")
+
+ax.plot3D(0, 0, 1, marker="d", markersize=2)
+
+f_line_x = np.linspace(1/3, 1/2, 1000)
+f_line_y = np.linspace(0, 0, 1000)
+f_line_z = np.linspace(0, 0, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, 'gray', linestyle="dotted",
+          linewidth=0.35)
+
+f_line_x = np.linspace(1/2, 1/2, 1000)
+f_line_y = np.linspace(0, 1/2, 1000)
+f_line_z = np.linspace(0, 0, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, 'gray', linestyle="dotted",
+          linewidth=0.35)
+
+f_line_x = np.linspace(1/2, 1/2, 1000)
+f_line_y = np.linspace(0, 0, 1000)
+f_line_z = np.linspace(0, 1/2, 1000)
+ax.plot3D(f_line_x, f_line_y, f_line_z, 'gray', linestyle="dotted",
+          linewidth=0.35)
+
+ax.text(-0.325, 0.15, 1, s='Ref. value', fontsize=5, zdir=None, color=c_case_0,
+        bbox=dict(facecolor='white', edgecolor=c_case_0, linewidth=0.1,
+                  boxstyle="round,pad=0.3"))
+
+ax.text(.15, 1, 0.1, s=r'$-49\%$', fontsize=5, zdir=None, color=c_case_C,
+        bbox=dict(facecolor='white', edgecolor=c_case_C, linewidth=0.25,
+                  boxstyle="round,pad=0.3"))
+
+ax.text(.65, 0.5, 0.1, s=r'$-34\%$', fontsize=5, zdir=None, color=c_case_B,
+        bbox=dict(facecolor='white', edgecolor=c_case_B, linewidth=0.25,
+                  boxstyle="round,pad=0.3"))
+
+ax.text(-0.02, 1/3, 1/3, s=r'$-25\%$', fontsize=5, zdir=None, color=c_case_A,
+        bbox=dict(facecolor='white', edgecolor=c_case_A, linewidth=0.25,
+                  boxstyle="round,pad=0.3"))
+
+ax.text(0.25, 0, 0.15, s=r'$-6\%$', fontsize=5, zdir=None, color=c_case_D,
+        bbox=dict(facecolor='white', edgecolor=c_case_D, linewidth=0.25,
+                  boxstyle="round,pad=0.3"))
+
+# zdirs = ('x', None)
+# xs = (0.5, 0)
+# ys = (0.5, 0)
+# zs = (0.5, 0)
+
+# for zdir, x, y, z in zip(zdirs, xs, ys, zs):
+#     label = '(%d, %d, %d), dir=%s' % (x, y, z, zdir)
+#     ax.text(x, y, z, label, zdir)
+    
+ax.set_zlabel("Governance's share $(g)$", fontsize=5, labelpad=-10)
+ax.set_xlabel("Tenant's share $(t)$", fontsize=5, labelpad=-10)
+ax.set_ylabel("Landlord's share $(l)$", fontsize=5, labelpad=-10)
+
+ax.set_xticks(ticks=[0, 1/3, 0.5, 2/3, 0.915])
+ax.set_xticklabels(
+    labels=[r'$0$', r'$\frac{1}{3}$', r'$\frac{1}{2}$', r'$\frac{2}{3}$', r'$1$'])
+ax.set_yticks(ticks=[0, 1/3, 0.5, 2/3, 1])
+ax.set_yticklabels(
+    labels=[r'$0$', r'$\frac{1}{3}$', r'$\frac{1}{2}$', r'$\frac{2}{3}$', r'$1$'],
+    ha="center", va="center")
+
+ax.set_zticks(ticks=[0, 1/3, 0.5, 2/3, 1])
+ax.set_zticklabels(
+    labels=[r'$0$', r'$\frac{1}{3}$', r'$\frac{1}{2}$', r'$\frac{2}{3}$', r'$1$'])
+
+offset = matplotlib.transforms.ScaledTranslation(1, 0, fig.dpi_scale_trans)
+
+for label in ax.yaxis.get_majorticklabels():
+    label.set_transform(label.get_transform() + offset)
+
+ax.tick_params(axis='y', which='major', pad=-4)
+ax.tick_params(axis='x', which='major', pad=-6)
+ax.tick_params(axis='z', which='major', pad=-4)
+
+
+
+
+from matplotlib.lines import Line2D
+_patches = []
+# _line = Line2D([1], [2], label = "Minimum",color="black")
+# _patches.extend([_line])
+# _line = Line2D([0], [0], label = "Heat",color="black", linewidth=6)
+# _patches.extend([_line])
+line4 = Line2D(range(1), range(1), color="white", marker='o',markersize=5,
+               markerfacecolor=c_case_0, label='GD (DH) ($t$=0, $l$=0, $g$=1)')
+_patches.extend([line4])
+
+line4 = Line2D(range(1), range(1), color="white", marker='o',markersize=5*0.75,
+               markerfacecolor=c_case_A, label=r'Case A $(\frac{1}{3}, \frac{1}{3}, \frac{1}{3})$')
+_patches.extend([line4])
+line4 = Line2D(range(1), range(1), color="white", marker='o',markersize=5*0.66,
+               markerfacecolor=c_case_B, label=r'Case B $(\frac{1}{2}, \frac{1}{2}, 0)$')
+_patches.extend([line4])
+line4 = Line2D(range(1), range(1), color="white", marker='o',markersize=5*0.51,
+               markerfacecolor=c_case_C, label='Case C $(0, 1, 0)$')
+_patches.extend([line4])
+line4 = Line2D(range(1), range(1), color="white", marker='o',markersize=5*0.94,
+               markerfacecolor=c_case_D, label=r'Case D $(\frac{1}{2}, 0, \frac{1}{2})$')
+_patches.extend([line4])
+leg = ax.legend(handles=_patches, loc='lower center', bbox_to_anchor=(0.625, 0.65), ncol=1, edgecolor="black", frameon=True, framealpha=1)
+leg.get_frame().set_linewidth(0.25)
+
+ax.set_title(
+    "Rel. change of objective value in \% of GD (DH)\nfor varying allocation of opportunity costs", fontsize=8, y=1.02)
+
+
+
+ax.scatter(0, 0, 1, s=200, color=c_case_0)
+ax.scatter(1/3, 1/3, 1/3, s=151, color=c_case_A)
+ax.scatter(1/2, 1/2, 0, s=133, color=c_case_B)
+ax.scatter(0, 1, 0, s=102, color=c_case_C)
+ax.scatter(1/2, 0, 1/2, s=188, color=c_case_D)
+
+ax.set_zlim(0, 1)
+ax.set_ylim(0, 1)
+ax.set_xlim(0, 1)
+# plt.tight_layout()
+fig.savefig("3d.png", dpi=900)
+fig.savefig("3d.eps", format="eps")
---
+++ manuscript/Figures/4_Results/fig_feasible/script_plot.py
@@ -0,0 +1,127 @@
+"""Plotting script the net present value of the six different sceanrios."""
+import pyam
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mlp
+import matplotlib.ticker as tkr
+import matplotlib.patches as mpatches
+
+plt.style.use(['science'])
+plt.rcParams['xtick.labelsize'] = 6
+plt.rcParams['ytick.labelsize'] = 6
+plt.rc('legend', fontsize=6)
+
+box_color= "#444444"
+
+data = pyam.IamDataFrame("mappe2.xlsx")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+c_edge = "#334257"
+
+
+cmap = plt.cm.jet  # define the colormap
+# extract all colors from the .jet map
+cmaplist = [cmap(i) for i in range(cmap.N)]
+# force the first color entry to be grey
+cmaplist[0] = (.5, .5, .5, 1.0)
+
+_c = ["#2F86A6", "#81B214", "#FF8303"]
+cmap = mlp.colors.LinearSegmentedColormap.from_list(
+    'Custom cmap', _c, cmap.N)
+
+data.plot(ax=ax, title=None, legend=False,
+          marker="|", markersize=2, cmap=cmap, color="scenario")
+
+
+ax.plot([2032, 2032], [30000, 93533*0.985], linestyle="solid",
+        color=box_color, linewidth=0.2)
+ax.plot([2030, 2032], [30000, 30000], linestyle="solid",
+        color=box_color, linewidth=0.2, zorder=-1)
+ax.text(x=2025.25, y=30000,
+        s="Feasibility limit $(s_l=70\%)$\nObj. value = 93.5kEUR",
+        fontsize=6, va="center", ha="left", color=box_color,
+        multialignment="left", linespacing=1.5,
+        bbox=dict(facecolor='white', edgecolor=box_color, linewidth=0.25,
+                              boxstyle="round,pad=0.3"))
+
+
+ax.plot([2031, 2031], [70000, 89877*0.985], linestyle="solid",
+        color=box_color, linewidth=0.2)
+ax.plot([2030, 2031], [70000, 70000], linestyle="solid",
+        color=box_color, linewidth=0.2, zorder=-1)
+ax.text(x=2029.5, y=173000,
+        s="Feasibility limit $(s_l=100\%)$\nObj. value = 99.4kEUR",
+        fontsize=6, va="center", ha="left", color=box_color,
+        multialignment="left", linespacing=1.5,
+        bbox=dict(facecolor='white', edgecolor=box_color, linewidth=0.25,
+                              boxstyle="round,pad=0.3"))
+
+
+ax.plot([2035, 2035], [173000, 99419*0.985], linestyle="solid",
+        color=box_color, linewidth=0.2)
+ax.plot([2030.5, 2035], [173000, 173000], linestyle="solid",
+        color=box_color, linewidth=0.2, zorder=-1)
+
+
+
+ax.text(x=2025.25, y=70000,
+        s="Feasibility limit $(s_l=60\%)$\nObj. value = 89.8kEUR",
+        fontsize=6, va="center", ha="left", color=box_color,
+        multialignment="left", linespacing=1.5,
+        bbox=dict(facecolor='white', edgecolor=box_color, linewidth=0.25,
+                              boxstyle="round,pad=0.3"))
+
+
+
+
+
+
+# ax.plot([2031, 2031], [0, 89877], linestyle="dashed", color="#99A799")
+ax.set_xticks([2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035])
+ax.set_xticklabels(labels=["0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"])
+ax.set_ylabel("")
+ax.set_xlabel("Landlord's share in costs of inaction $(s_l)$", fontsize=8)
+
+
+
+ax.set_title("Objective value in EUR for varying landlord's interest rate",
+                         fontsize=8)
+
+ax.set_ylim([0, 220000])
+group_thousands = tkr.FuncFormatter(lambda x, pos: '{:0,d}'.format(
+    int(x)).replace(',', ' '))
+
+ax.yaxis.set_major_formatter(group_thousands)
+
+ax.tick_params(axis='x',  which='both', top=False)
+ax.tick_params(axis='y',  which='both', right=False)
+
+leg = ax.legend(loc='upper right', framealpha=1, handlelength=1,
+                handletextpad=1, borderpad=0.25, columnspacing=1,
+                edgecolor=None, frameon=False, ncol=3, facecolor=None)
+leg.get_frame().set_linewidth(0.25)
+
+leg.get_texts()[0].set_text(r'$i_l=10\%$')
+leg.get_texts()[1].set_text(r'$i_l=5\%$')
+leg.get_texts()[2].set_text(r'$i_l=3\%$')
+
+ellipse = mpatches.Ellipse(xy=(2033.75, 50000), width=2.5, height=30000,
+                           facecolor="white", linewidth=0.5, edgecolor="black",
+                           linestyle="dashed")
+ax.add_patch(ellipse)
+ax.text(x=2033.75, y=50000, s=r'Infeasible', color="black", va="center",
+        ha="center", fontsize=7)
+
+ax.plot([2031, 2032, 2035], [89877, 93533, 99419], color="black",
+        linestyle="dashed", zorder=-100, linewidth=0.5)
+
+# ax.text(x=2032.25, y=20000, s=r'$\longleftarrow$ Decreaseing $i_l$ extends infeasible', fontsize=4)
+
+# ax.plot([2031, 2032], [89877, 93533], linestyle="dashed",
+#         color="red", linewidth=1, zorder=-1)
+
+plt.tight_layout()
+fig.savefig("feasible.eps", format="eps")
+fig.savefig("feasible.png", dpi=900)
